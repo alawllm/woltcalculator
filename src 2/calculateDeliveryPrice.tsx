91c9e6df9f972477586b3d5116ed0ac5de69090a
alawllm <alicja.willam@gmail.com>
2023-01-26 19:38:19 +0100

    deleted folder that got duplicated before

--- src 2/calculateDeliveryPrice.tsx
+++
@@ -1,6 +0,0 @@
-const calculateDeliveryPrice = function (distanceMeters: number, basketValue: number, datetime: any): number {
-    alert(`INNER FUNCTION ALERT, HAHA`)
-    return 5;
-}
-
-export default calculateDeliveryPrice
--- src 2/components/Abc.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './Abc.css';
-
-export default class Abc extends React.Component {
-    render() {
-        return <p className="Abc-paragraph"> ABC!!!! </p>;
-    }
-}
