75c40d1cdfdb452aa90ee48f533ccf61ba386409
alawllm <alicja.willam@gmail.com>
2023-01-26 20:12:16 +0100

    cleaned up datetime handling

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -11,13 +11,13 @@ const Calculator: React.FC = (): ReactElement => {
     const [distance, setDistance] = useState<number>(0);
     const [numberItems, setNumberItems] = useState<number>(0);
     const [basketValue, setBasketValue] = useState<number>(0);
-    const [datetime, setDatetime] = useState<any>('');
+    const [datetime, setDatetime] = useState<string>('');
     const [outText, setOutText] = useState<string>('');
 
     const handleButtonClick = function (): void {
         let output_text: string;
         const deliveryPrice = calculateDeliveryPrice(distance, numberItems, basketValue, datetime)
-        output_text = `distance=${distance}, numberItems=${numberItems} basketValue=${basketValue}, datetime=${datetime.toString()} \n delivery price=${deliveryPrice}`
+        output_text = `distance=${distance}, numberItems=${numberItems} basketValue=${basketValue}, datetime=${datetime} \n delivery price=${deliveryPrice}`
         setOutText(output_text)
     }
 
@@ -32,7 +32,7 @@ const Calculator: React.FC = (): ReactElement => {
                     onChangeDistance={(e) => setDistance(parseInt(e.target.value))}
                     onChangeNumberItems={(e) => setNumberItems(parseInt(e.target.value))}
                     onChangeBasketValue={(e) => setBasketValue(parseInt(e.target.value))}
-                    onChangeDatetime={(e) => setDatetime(e.target.value)}
+                    onChangeDatetime={(e) => setDatetime(e.target.value.toString())}
                     handleButtonClick={handleButtonClick}
                 />
                 <OutputField
